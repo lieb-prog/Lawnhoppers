cc7d52bd32aa0fd43f2ef5ff1c4b56db2b934ecf
lieb-prog <reuben@liebandweiss.com>
2026-05-26 15:49:52 -0500

    Add Tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,jsx}'],
+  theme: {
+    extend: {
+      colors: {
+        ink: '#063447',
+        lawn: '#2f7d32',
+        cream: '#efede8'
+      }
+    }
+  },
+  plugins: []
+}
